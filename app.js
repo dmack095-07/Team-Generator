949411647666ef0ec76df4b04fcf01e05946373d
Dianna Mack <dianna.mack@yahoo.com>
2020-09-01 22:19:53 -0400

    added css/updated code

--- app.js
+++ app.js
@@ -4,32 +4,100 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const employee =[];
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function promptUser() {
+  return inquirer.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?"
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your id number?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+  },
+  {
+    type: "checkbox",
+    name: "role",
+    message: "Select your role?",
+    choices:[
+      "Intern",
+      "Engineer",
+      "Manager",
+    ],
+  },
+]);
+} 
+ function generateHTML(answers) {
+   return `
+   <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+  </head>
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+  <body>
+    <div class="container-fluid" >
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-heading" id = "header">
+                <h1 class="text-center">My Team</h1>
+            </div>
+            {{ team }}
+        </div>
+    </div>
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+  </body>
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+  </html>`;
+ }
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+  
+  
+  promptUser()
+  .then(function(employees){
+    const html = generateHTML(employees);
+
+    return writeFileAsync("./output/team.html", html);
+  })
+  .then(function(){
+    console.log("The employees are added to team.html");
+  })
+  .catch(function(err){
+    console.log(err);
+  });
+
+  
+    
+
+    
+
+ 
+  
+
